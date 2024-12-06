d1e28640b6aca53cc706b8c9b3676c9e6f150bb9
Mortimer, Nicolas (Environment, IOMRC Crawley) <nick.mortimer@csiro.au>
2024-12-06 15:23:08 +1100

    basic pipeline

---
+++ barcode-pipeline.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Tuple
+
+from ifdo.models import ImageData
+
+from marimba.core.pipeline import BasePipeline
+
+
+class MyPipeline(BasePipeline):
+
+    @staticmethod
+    def get_pipeline_config_schema() -> dict:
+        return {}
+
+    @staticmethod
+    def get_collection_config_schema() -> dict:
+        return {}
+
+    def _import(
+        self,
+        data_dir: Path,
+        source_path: Path,
+        config: Dict[str, Any],
+        **kwargs: dict,
+    ):
+
+        return
+
+    def _process(
+        self,
+        data_dir: Path,
+        config: Dict[str, Any],
+        **kwargs: dict,
+    ):
+
+        return
+
+    def _package(
+        self,
+        data_dir: Path,
+        config: Dict[str, Any],
+        **kwargs: dict,
+    ) -> Dict[Path, Tuple[Path, Optional[List[ImageData]], Optional[Dict[str, Any]]]]:
+
+        data_mapping: Dict[Path, Tuple[Path, Optional[List[ImageData]], Optional[Dict[str, Any]]]] = {}
+        return data_mapping
